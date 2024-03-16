08c8a2d9799bb82dc9a5b820dd5112a44a91f49b
gaoxipei <gaoxipei@outlook.com>
2024-03-17 03:39:04 +0800

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+ 
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8,u8,u8),
+    Quit,
+    Echo(String),
+    Move(Point),
 }
 
 struct Point {
@@ -43,6 +47,12 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message{
+            Message::ChangeColor(x,y,z) => self.color =(x,y,z),
+            Message::Quit => self.quit = true,
+            Message::Echo(string) => self.message = string,
+            Message::Move(Point{x, y}) => self.position = Point{x, y},
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,12 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+ 
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -34,11 +35,15 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        scores.entry(team_1_name).and_modify(|team| *team = Team{goals_scored: team.goals_scored + team_1_score, goals_conceded: team.goals_conceded + team_2_score})
+            .or_insert(Team{goals_scored:  team_1_score, goals_conceded: team_2_score});
+        scores.entry(team_2_name).and_modify(|team1| *team1 = Team{goals_scored: team1.goals_scored + team_2_score, goals_conceded: team1.goals_conceded + team_1_score} )
+            .or_insert(Team{goals_scored:  team_2_score, goals_conceded:  team_1_score});
+
+
+
+
+     
     }
     scores
 }
